d0dad28294a1c234de2b55263588334e6f7433a0
Brianna Peterson <89970458+Bricheese@users.noreply.github.com>
2021-09-30 21:51:09 -0700

    Skills Assessment Progress

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,52 @@
-// HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Brianna Peterson"
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
 
-// Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+//gb
 
-// Event listener for clicks on the "+" button for Ginger Bread cookies
+
+//cc
+let ccTbRow = document.querySelector('#qty-cc')
+
+//sugar
+
+
+
+document.getElementById('credit').textContent = `Created by ${'Brianna Peterson'}`
+
+//button clicks logged
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
+    alert("Your order now contains one more Ginger Bread cookie!") //Just a placeholder
     console.log('Ginger bread + button was clicked!')
+});
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
-})
+document.getElementById('minus-gb').addEventListener('click', function() {
+    alert("Your order now contains one less Ginger Bread cookie!") //Just a placeholder
+    console.log('Ginger bread - button was clicked!')
+});
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('add-cc').addEventListener('click', function() {
+    cc = cc + 1;
+    ccTbRow.textcontent = cc;
+    console.log('Chocolate Chip + button was clicked!')
+});
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    cc = cc - 1;
+    ccTbRow.textcontent = cc;
+    console.log('Chocloate Chip - button was clicked!')
+});
+
+document.getElementById('add-sugar').addEventListener('click', function() {
+    console.log('Sugar Sprinkle + button was clicked!')
+});
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    console.log('Sugar Sprinkle - button was clicked!')
+});
